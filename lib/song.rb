296d26407b6e73a207a1b3afb5e09ad69d8cc097
Josh Ellis <joshe523@gmail.com>
2020-03-04 05:39:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,20 @@
 class Song
   attr_accessor :name, :artist, :genre
 
+  @@count = 0
+  @@artist = []
+  @@genres = []
+
   def initialize(name, artist, genre)
+    @@count += 1
     @name = name
     @artist = artist
+    @@artist << artist
     @genre = genre
+    @@genre << genre
+  end
+
+  def count 
+    @@count
   end
 end
