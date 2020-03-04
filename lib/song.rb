add29afa3aa40e329cf9839a2b529c4b8d1efca2
Josh Ellis <joshe523@gmail.com>
2020-03-04 05:48:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     genre_count
   end
 
-  def artist_count
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
        artist_count[artist] ? artist_count[artist] += 1 : artist_count[artist] = 1
