f502da0272dca6a200c8b10abb41e377c019f4ed
Josh Ellis <joshe523@gmail.com>
2020-03-04 05:47:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-       genre_count[genre] ? genre_count[genre].value += 1 : genre_count[genre].value = 1
+       genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1
     end
   end
 
